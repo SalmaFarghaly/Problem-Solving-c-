7c4c28febfc65901cc84976c8d4085b8e987db1d
SalmaFarghaly <59613960+SalmaFarghaly@users.noreply.github.com>
2022-02-10 19:58:33 +0200

    Create OrderingTasks.cpp

---
+++ Level-A/GraphTheory/OrderingTasks.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<list>
+#include<stack>
+
+stack<int> st;
+
+void DFS(list<int>* adj, vector<bool>& vis,int node) {
+	vis[node] = true;
+	for (auto it = adj[node].begin(); it != adj[node].end(); it++) {
+		if(! vis[*it])
+			DFS(adj, vis, *it);
+	}
+	st.push(node);
+}
+
+int main() {
+	int N = 1, M = 1;
+	while (true) {
+		cin >> N >> M;
+		if (N == 0 && M == 0) break;
+		list<int>* adj = new list<int>[N];
+		while (M--) {
+			int u, v;
+			cin >> u >> v;
+			adj[u - 1].push_back(v - 1);
+		}
+		vector<bool> vis(N, 0);
+		for (int i = 0; i < N; i++) {
+			if (!vis[i])
+				DFS(adj, vis, i);
+		}
+		while (!st.empty()) {
+			cout << st.top() + 1 << " ";
+			st.pop();
+		}
+		cout << endl;
+	}
+	return 0;
+}

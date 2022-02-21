9134515a076f3e2764ee704a034e9f3c90a3b070
SalmaFarghaly <59613960+SalmaFarghaly@users.noreply.github.com>
2022-02-21 17:45:22 +0200

    Update BurningMidnightOil.cpp

--- Level-A/BinarySearch/BurningMidnightOil.cpp
+++ Level-A/BinarySearch/BurningMidnightOil.cpp
@@ -1,58 +1,38 @@
 //https://codeforces.com/contest/165/problem/B
 
 #include<iostream>
-#include<climits>
 using namespace std;
-
-typedef long long ll;
-
-
-int isValid(ll n,ll v,int k)
-{
-	int p = 0;
-	ll cnt = 0;
-	ll result =pow(k,p);
-	while (v>=result&&cnt<n)
-	{
-		cnt+=v/result;
-		p++;
-		result = pow(k, p);
+#include<algorithm>
+ 
+int n;
+int k;
+ 
+bool isValid(int ele) {
+	int pages = n-ele;
+	int tempK = k;
+	while (pages > 0 && floor(ele/tempK) != 0) {
+		pages -= ele / tempK;
+		tempK *= k;
 	}
-	if (cnt >= n)
-		return 1;
-	else
-		return 0;
+	return pages <= 0;
 }
-ll BS(ll n,int k)
-{
-	ll lastvalid = INT_MAX;
-	ll st = 1;
-	ll ed = n;
-	ll md;
-	while (st<ed)
-	{
-		md = (st + ed) / 2;
-		int Cnt=isValid(n,md,k);
-		if (Cnt == 0)
-			st = md + 1;
-		else
-		{
-			ed = md - 1;
-			lastvalid = md;
-		}
-	}
-	if (isValid(n, st, k) && lastvalid > st)
-		return st;
-	else
-	    return lastvalid;
-}
-
-int main()
-{
-	ll n;
-	int k;
+  
+int main() {
 	cin >> n >> k;
-	if (n == 1)cout << 1;
-	else cout<<BS(n,k);
+	int st = 1;
+	int ed = n;
+	int minValid = INT_MAX;
+	while (st <= ed) {
+		int mid = (st + ed) / 2;
+		if (isValid(mid)) {
+			minValid = min(minValid, mid);
+			ed = mid - 1;
+		}
+		else {
+			st = mid + 1;
+		}
+ 
+	}
+	cout << minValid;
 	return 0;
 }

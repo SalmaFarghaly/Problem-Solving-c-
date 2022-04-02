723aae8b11629d7592943aef9439b765faa96a41
SalmaFarghaly <59613960+SalmaFarghaly@users.noreply.github.com>
2022-04-02 20:36:38 +0200

    Update TOE(1).cpp

--- Level-B/GraphTheory/TOE(1).cpp
+++ Level-B/GraphTheory/TOE(1).cpp
@@ -1,12 +1,42 @@
 // https://www.spoj.com/problems/TOE1/
 
-#include<iostream>
+#include<set>
+#include<map>
+#include<list>
+#include<iomanip>
+#include<cmath>
 #include<string>
+#include<vector>
 #include<queue>
+#include<stack>
+#include<complex>
+#include<sstream>
+#include<iostream>
+#include<fstream>
 #include<algorithm>
+#include<numeric>
+#include<utility>
+#include<functional>
+#include<stdio.h>
+#include<assert.h>
+#include<memory.h>
+#include<bitset>
 using namespace std;
 
 #define loop(i,n) for(int i=0;i<n;i++)
+#define all(v)				((v).begin()), ((v).end())
+#define sz(v)				((int)((v).size()))
+#define clr(v, d)			memset(v, d, sizeof(v))
+#define rep(i, v)		for(int i=0;i<sz(v);++i)
+#define lp(i, n)		for(int i=0;i<(int)(n);++i)
+#define lpi(i, j, n)	for(int i=(j);i<(int)(n);++i)
+#define lpd(i, j, n)	for(int i=(j);i>=(int)(n);--i)
+
+#define MP					make_pair
+
+typedef vector<int> vi;
+typedef vector<vector<int>> vii;
+typedef pair<string, int> pii;
 
 bool isWinState(string board, char c) {
 
@@ -24,13 +54,48 @@ bool isWinState(string board, char c) {
 	return row || col || diagonal;
 }
 
-int main() {
-
+void solWithBFS() {
 	int t;
 	cin >> t;
-  // 1) solution without BFS
 	while (t--) {
-		string target = "",row;
+		string target = "", start = ".........", ans = "no", row;
+		loop(i, 3) {
+			cin >> row;
+			target += row;
+		}
+		queue<pii> qu;
+		qu.push(MP(start, 1));
+		while (!qu.empty()) {
+			string board = qu.front().first;
+			int turn = qu.front().second;
+			if (board == target) {
+				ans = "yes";
+				break;
+			}
+			if (isWinState(board, 'X') || isWinState(board, 'O')) {
+				qu.pop();
+				continue;
+			}
+			loop(i, 9) {
+				if (board[i] == '.') {
+					board[i] = (turn == 1) ? 'X' : 'O';
+					if (board[i] == target[i]) {
+						qu.push(MP(board, 1 - turn));
+					}
+					board[i] = '.';
+				}
+			}
+			qu.pop();
+		}
+		cout << ans << endl;
+	}
+}
+
+void solWOBFS() {
+	int t;
+	cin >> t;
+	while (t--) {
+		string target = "", row;
 		loop(i, 3) {
 			cin >> row;
 			target += row;
@@ -38,18 +103,26 @@ int main() {
 		int count_x = count(target.begin(), target.end(), 'X');
 		int count_o = count(target.begin(), target.end(), 'O');
 		string ans = "yes";
-		if (count_x - count_o > 1 || count_x < count_o )
+		// number of X's on board at any time should be equal #O's or greater than #O's by exactly 1
+		if (count_x - count_o > 1 || count_x < count_o)
 			ans = "no";
+		// X and O can't win at the same time
 		else if (isWinState(target, 'X') && isWinState(target, 'O'))
 			ans = "no";
+		// if X is a win then #X's should be greater than #O's by 1
 		else if (isWinState(target, 'X') && count_x - count_o != 1)
 			ans = "no";
+		// if O is a win then #X's should be equal to #O'ss
 		else if (isWinState(target, 'O') && count_x != count_o)
 			ans = "no";
-		cout << ans <<endl;
+		cout << ans << endl;
 	}
+}
 
-
-
+int main() {
+	// 1) solution with BFS
+	solWithBFS();
+	// 2) solution without BFS
+	solWOBFS();
 	return 0;
 }
